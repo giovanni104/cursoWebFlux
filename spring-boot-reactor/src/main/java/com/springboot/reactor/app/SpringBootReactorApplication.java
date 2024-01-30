8f85cc91c338f503e37f568dba641262a66ff714
Giovanny Hernandez Arbelaez <giovanni104@hotmail.com>
2024-01-29 22:31:31 -0500

    avances del curso

--- spring-boot-reactor/src/main/java/com/springboot/reactor/app/SpringBootReactorApplication.java
+++ spring-boot-reactor/src/main/java/com/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,16 @@
 package com.springboot.reactor.app;
 
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.CountDownLatch;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,36 +22,160 @@ import com.springboot.reactor.app.models.UsuarioComentarios;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
-public class SpringBootReactorApplication implements CommandLineRunner{
+public class SpringBootReactorApplication implements CommandLineRunner {
 
 	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		 
-		//ejemploIterable();
-		
-		// ejemploFlatMap();
-		// ejemploToString();
-		ejemploCollectList();
-		
+
+		ejemploZipWithRangos();
+
 	}
-	
+
+	public void ejemploContraPresion() {
+
+		Flux.range(1, 10).log()
+				// .limitRate(5)
+				.subscribe(new Subscriber<Integer>() {
+
+					private Subscription s;
+
+					private Integer limite = 5;
+					private Integer consumido = 0;
+
+					@Override
+					public void onSubscribe(Subscription s) {
+						this.s = s;
+						s.request(limite);
+					}
+
+					@Override
+					public void onNext(Integer t) {
+						log.info(t.toString());
+						consumido++;
+						if (consumido == limite) {
+							consumido = 0;
+							s.request(limite);
+						}
+					}
+
+					@Override
+					public void onError(Throwable t) {
+						// TODO Auto-generated method stub
+
+					}
+
+					@Override
+					public void onComplete() {
+						// TODO Auto-generated method stub
+
+					}
+				});
+	}
+
+	public void ejemploIntervalDesdeCreate() {
+		Flux.create(emitter -> {
+			Timer timer = new Timer();
+			timer.schedule(new TimerTask() {
+
+				private Integer contador = 0;
+
+				@Override
+				public void run() {
+					emitter.next(++contador);
+					if (contador == 10) {
+						timer.cancel();
+						emitter.complete();
+					}
+
+					if (contador == 5) {
+						timer.cancel();
+						emitter.error(new InterruptedException("Error, se ha detenido el flux en 5!"));
+					}
+
+				}
+			}, 1000, 1000);
+		}).subscribe(next -> log.info(next.toString()), error -> log.error(error.getMessage()),
+				() -> log.info("Hemos terminado"));
+	}
+
+	public void ejemploIntervalInfinito() throws InterruptedException {
+
+		CountDownLatch latch = new CountDownLatch(1);
+
+		Flux.interval(Duration.ofSeconds(1)).doOnTerminate(latch::countDown).flatMap(i -> {
+			if (i >= 5) {
+				return Flux.error(new InterruptedException("Solo hasta 5!"));
+			}
+			return Flux.just(i);
+		}).map(i -> "Hola " + i).retry(2).subscribe(s -> log.info(s), e -> log.error(e.getMessage()));
+
+		latch.await();
+	}
+
+	public void ejemploDelayElements() {
+		Flux<Integer> rango = Flux.range(1, 12).delayElements(Duration.ofSeconds(1))
+				.doOnNext(i -> log.info(i.toString()));
+
+		rango.blockLast();
+	}
+
+	public void ejemploInterval() {
+		Flux<Integer> rango = Flux.range(1, 12);
+		Flux<Long> retraso = Flux.interval(Duration.ofSeconds(1));
+
+		rango.zipWith(retraso, (ra, re) -> ra).doOnNext(i -> log.info(i.toString())).blockLast();
+	}
+
+	public void ejemploZipWithRangos() {
+		Flux<Integer> rangos = Flux.range(0, 4);
+		Flux.just(1, 2, 3, 4).map(i -> (i * 2))
+				.zipWith(rangos, (uno, dos) -> String.format("Primer Flux: %d, Segundo Flux: %d", uno, dos))
+				.subscribe(texto -> log.info(texto));
+	}
+
+	public void ejemploUsuarioComentariosZipWithForma2() {
+		Mono<Usuario> usuarioMono = Mono.fromCallable(() -> new Usuario("John", "Doe"));
+
+		Mono<Comentarios> comentariosUsuarioMono = Mono.fromCallable(() -> {
+			Comentarios comentarios = new Comentarios();
+			comentarios.addComentario("Hola pepe, qué tal!");
+			comentarios.addComentario("Mañana voy a la playa!");
+			comentarios.addComentario("Estoy tomando el curso de spring con reactor");
+			return comentarios;
+		});
+
+		Mono<UsuarioComentarios> usuarioConComentarios = usuarioMono.zipWith(comentariosUsuarioMono).map(tuple -> {
+			Usuario u = tuple.getT1();
+			Comentarios c = tuple.getT2();
+			return new UsuarioComentarios(u, c);
+		});
+
+		usuarioConComentarios.subscribe(uc -> log.info(uc.toString()));
+	}
+
+	public void ejemploUsuarioComentariosZipWith() {
+		Mono<Usuario> usuarioMono = Mono.fromCallable(() -> new Usuario("John", "Doe"));
+
+		Mono<Comentarios> comentariosUsuarioMono = Mono.fromCallable(() -> {
+			Comentarios comentarios = new Comentarios();
+			comentarios.addComentario("Hola pepe, qué tal!");
+			comentarios.addComentario("Mañana voy a la playa!");
+			comentarios.addComentario("Estoy tomando el curso de spring con reactor");
+			return comentarios;
+		});
+
+		Mono<UsuarioComentarios> usuarioConComentarios = usuarioMono.zipWith(comentariosUsuarioMono,
+				(usuario, comentariosUsuario) -> new UsuarioComentarios(usuario, comentariosUsuario));
+
+		usuarioConComentarios.subscribe(uc -> log.info(uc.toString()));
+	}
 
 	public void ejemploUsuarioComentariosFlatMap() {
 		Mono<Usuario> usuarioMono = Mono.fromCallable(() -> new Usuario("John", "Doe"));
@@ -57,8 +192,7 @@ public class SpringBootReactorApplication implements CommandLineRunner{
 				.flatMap(u -> comentariosUsuarioMono.map(c -> new UsuarioComentarios(u, c)));
 		usuarioConComentarios.subscribe(uc -> log.info(uc.toString()));
 	}
-	
-	
+
 	public void ejemploCollectList() throws Exception {
 
 		List<Usuario> usuariosList = new ArrayList<>();
@@ -98,9 +232,7 @@ public class SpringBootReactorApplication implements CommandLineRunner{
 					return nombre.toLowerCase();
 				}).subscribe(u -> log.info(u.toString()));
 	}
-	
-	
-	
+
 	public void ejemploFlatMap() throws Exception {
 
 		List<String> usuariosList = new ArrayList<>();
@@ -126,7 +258,7 @@ public class SpringBootReactorApplication implements CommandLineRunner{
 					return usuario;
 				}).subscribe(u -> log.info(u.toString()));
 	}
-	
+
 	public void ejemploIterable() throws Exception {
 
 		List<String> usuariosList = new ArrayList<>();
@@ -167,12 +299,5 @@ public class SpringBootReactorApplication implements CommandLineRunner{
 		});
 
 	}
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
