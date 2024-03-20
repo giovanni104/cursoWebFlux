cd9cb0252cd0aa2484652b91815bcd29ca74e776
Giovanny Hernandez Arbelaez <46624723+giovanni104@users.noreply.github.com>
2024-03-20 17:17:16 -0500

    avances curso

--- spring-boot-webflux/src/main/java/com/pyt/springboot/webflux/controllers/ProductoController.java
+++ spring-boot-webflux/src/main/java/com/pyt/springboot/webflux/controllers/ProductoController.java
@@ -3,8 +3,9 @@ package com.pyt.springboot.webflux.controllers;
 
 import java.time.Duration;
 
-import com.pyt.springboot.webflux.models.dao.ProductoDao;
+
 import com.pyt.springboot.webflux.models.documents.Producto;
+import com.pyt.springboot.webflux.models.services.ProductoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,26 +13,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.thymeleaf.spring6.context.webflux.ReactiveDataDriverContextVariable;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 
 @Controller
 public class ProductoController {
 
     @Autowired
-    private ProductoDao dao;
+    private ProductoService service;
 
     private static final Logger log = LoggerFactory.getLogger(ProductoController.class);
 
     @GetMapping({"/listar", "/"})
     public String listar(Model model) {
 
-        Flux<Producto> productos = dao.findAll().map(producto -> {
-
-            producto.setNombre(producto.getNombre().toUpperCase());
-            return producto;
-        });
+        Flux<Producto> productos = service.findAllConNombreUpperCase();
 
         productos.subscribe(prod -> log.info(prod.getNombre()));
 
@@ -40,14 +39,32 @@ public class ProductoController {
         return "listar";
     }
 
+
+    @GetMapping("/form")
+    public Mono<String> crear(Model model) {
+        model.addAttribute("producto", new Producto());
+        model.addAttribute("titulo", "Formulario de producto");
+        model.addAttribute("boton", "Crear");
+        return Mono.just("form");
+    }
+
+
+    @PostMapping("/form")
+    public Mono<String> guardar(Producto producto) {
+
+
+        return service.save(producto)
+                .doOnNext(p -> {
+                    log.info("Producto guardado: " + p.getNombre() + " Id: " + p.getId());
+                }).thenReturn("redirect:/listar");
+
+    }
+
+
     @GetMapping("/listar-datadriver")
     public String listarDataDriver(Model model) {
 
-        Flux<Producto> productos = dao.findAll().map(producto -> {
-
-            producto.setNombre(producto.getNombre().toUpperCase());
-            return producto;
-        }).delayElements(Duration.ofSeconds(1));
+        Flux<Producto> productos = service.findAllConNombreUpperCase().delayElements(Duration.ofSeconds(1));
 
         productos.subscribe(prod -> log.info(prod.getNombre()));
 
@@ -56,14 +73,10 @@ public class ProductoController {
         return "listar";
     }
 
-   @GetMapping("/listar-full")
+    @GetMapping("/listar-full")
     public String listarFull(Model model) {
 
-        Flux<Producto> productos = dao.findAll().map(producto -> {
-
-            producto.setNombre(producto.getNombre().toUpperCase());
-            return producto;
-        }).repeat(5000);
+        Flux<Producto> productos = service.findAllConNombreUpperCaseRepeat();
 
         model.addAttribute("productos", productos);
         model.addAttribute("titulo", "Listado de productos");
@@ -73,11 +86,8 @@ public class ProductoController {
     @GetMapping("/listar-chunked")
     public String listarChunked(Model model) {
 
-        Flux<Producto> productos = dao.findAll().map(producto -> {
+        Flux<Producto> productos = service.findAllConNombreUpperCaseRepeat();
 
-            producto.setNombre(producto.getNombre().toUpperCase());
-            return producto;
-        }).repeat(5000);
 
         model.addAttribute("productos", productos);
         model.addAttribute("titulo", "Listado de productos");
