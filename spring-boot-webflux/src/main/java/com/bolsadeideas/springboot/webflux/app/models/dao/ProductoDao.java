36413999221b40ee0c63af6a90187c9e992e5ac4
Giovanny Hernandez Arbelaez <46624723+giovanni104@users.noreply.github.com>
2024-05-23 16:31:38 -0500

    avances del curso

--- spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/app/models/dao/ProductoDao.java
+++ spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/app/models/dao/ProductoDao.java
@@ -1,10 +1,8 @@
-package com.pyt.springboot.webflux.models.dao;
+package com.bolsadeideas.springboot.webflux.app.models.dao;
 
-
-import com.pyt.springboot.webflux.models.documents.Producto;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
-
+import com.bolsadeideas.springboot.webflux.app.models.documents.Producto;
 
 public interface ProductoDao extends ReactiveMongoRepository<Producto, String>{
 
